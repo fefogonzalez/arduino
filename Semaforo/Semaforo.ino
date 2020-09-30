a9dc7ddafbec28db370bd94f6196b063da48f2f0
fefogonzalez <42825387+fefogonzalez@users.noreply.github.com>
2020-09-30 00:56:47 -0300

    Update Semaforo.ino

--- Semaforo/Semaforo.ino
+++ Semaforo/Semaforo.ino
@@ -1,22 +1,22 @@
 void setup()
 {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT); // Led Rojo conectado al pin 2
+  pinMode(3, OUTPUT); // Led Amarillo conectado al pin 3
+  pinMode(4, OUTPUT); // Led Verde conectado al pin 4
 }
 
 void loop()
 {
-  digitalWrite(2, HIGH);
-  delay(3000); // Wait for 3000 millisecond(s)
-  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); // Se enciende led Rojo
+  delay(3000); // Durante 3000 milisegundos el led queda encendido
+  digitalWrite(2, LOW); // Se apaga led Rojo
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(3, HIGH);
-  delay(3000); // Wait for 3000 millisecond(s)
-  digitalWrite(3, LOW);
+  digitalWrite(3, HIGH); // Se enciende led Amarillo
+  delay(3000); // Durante 3000 milisegundos el led queda encendido
+  digitalWrite(3, LOW); // Se apaga led Amarillo
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(4, HIGH);
-  delay(3000); // Wait for 3000 millisecond(s)
-  digitalWrite(4, LOW);
-  delay(5000); // Wait for 5000 millisecond(s)
+  digitalWrite(4, HIGH); // Se enciende led Verde
+  delay(3000); // Durante 3000 milisegundos el led queda encendido
+  digitalWrite(4, LOW); // Se apaga led Verde
+  delay(5000); // Esperar 5000 milisegundos para que se vuelva a encender el led Rojo
 }
